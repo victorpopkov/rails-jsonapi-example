931301bb07acdeb1885a39b149ebfff95b641c14
Victor Popkov <victor@popkov.me>
2018-05-10 16:15:10 +0300

    Enable custom User-Agent in Page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,18 +4,19 @@ require 'nokogiri'
 require 'open-uri'
 
 class Page < ApplicationRecord
+  ALLOWED_PARSE_TAGS = %w(h1 h2 h3 a)
+  USER_AGENT         = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
+
   has_many :tags, class_name: 'PageTag'
 
   validates :url, url: true
-
-  # The list of all allowed tags to be parsed
-  ALLOWED_PARSE_TAGS = %w(h1 h2 h3 a)
 
   after_create :parse
 
   # Parses the remote page using the provided `url`.
   def parse
-    doc = Nokogiri::HTML(open(self.url))
+    html = open(self.url, 'User-Agent' => USER_AGENT)
+    doc  = Nokogiri::HTML(html)
 
     ALLOWED_PARSE_TAGS.each do |tag|
       doc.css(tag).each do |t|
@@ -37,6 +38,7 @@ class Page < ApplicationRecord
   #
   #   create_tag('a', 'Hello World')
   #   create_tag('h1', 'Heading 1')
+  #
   def create_tag(name, value)
     tag       = PageTag.new
     tag.page  = self
